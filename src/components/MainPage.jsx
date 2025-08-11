2a829533546ed23b7eb6950adeddece5e4155b76
akshay-ken <akshaybest3120@gmail.com>
2025-08-11 18:45:09 +0530

    feat: notification component ready for dynamic data maping

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,7 +3,7 @@ import { NotificationCard } from "./NotificationCard";
 
 export function MainPage() {
   return (
-    <main className="px-[4%] py-[8%] md:px-[20%] md:py-[8%]">
+    <main className="px-[4%] py-[8%] md:px-[20%] md:py-[8%] flex flex-col">
       <HeaderSection />
       <NotificationCard />
     </main>
