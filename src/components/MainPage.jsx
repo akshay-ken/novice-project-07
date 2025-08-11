c4b23a59abebb94896703d82188067470f12c031
akshay-ken <akshaybest3120@gmail.com>
2025-08-11 18:11:41 +0530

    feat: use notification component in mainpage.jsx file

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,11 @@
 import { HeaderSection } from "./HeaderSection";
+import { NotificationCard } from "./NotificationCard";
 
 export function MainPage() {
   return (
     <main className="px-[4%] py-[8%] md:px-[20%] md:py-[8%]">
       <HeaderSection />
+      <NotificationCard />
     </main>
   );
 }
