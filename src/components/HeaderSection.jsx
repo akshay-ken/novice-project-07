c185976ca9b51ef6cf56c921a28a2086ca825517
akshay-ken <akshaybest3120@gmail.com>
2025-08-11 17:47:42 +0530

    style: use custom font in layer base direcitve on body element

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -2,7 +2,9 @@ export function HeaderSection() {
   return (
     <header className="flex flex-row justify-between">
       <div className="flex flex-row justify-start items-center">
-        Notification <span className="">3</span>
+        <p>
+          Notification <span className="">3</span>
+        </p>
       </div>
       <p>Mark all as read</p>
     </header>
